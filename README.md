6c15ad873588f53fb1e4d2e79701ae6aa140bc37
Robert Paschedag <robert.paschedag@web.de>
2017-10-17 14:55:20 +0200

    added spacewalk-add-debian-multiarch-header.py

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ spacewalk-debian-sync
 =====================
 
 An improvised repo-sync to bring Debian packages into Spacewalk
+
+spacewalk-add-debian-multiarch-header.py
+========================================
+
+Script to add missing 'Multi-Arch' headers into the Packages files
+You need to use this modified version of Steve Meiers spacewalk-debian-sync
+script, which will create a temporary file within /tmp with
+
+- `<name> <version> <architecture> <multi-arch>`
+
+for each package with a Multi-Arch header found, while the Packages files
+gets parsed.
---
+++ spacewalk-add-debian-multiarch-header.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python -tt
+
+# Add missing "Multi-Arch" headers to "Packages" file
+# You have to use my modified version of Steve Meiers
+# spacewalk-debian-sync script, so the Multi-Arch headers
+# get written to a temporary file while the original
+# Packages file gets parsed
+#
+# You also need python-debian (which is installed on
+# Spacewalk server) to run this script
+#
+# Author: Robert Paschedag
+# Version: 20171017
+
+import sys
+from debian.deb822 import *
+
+if len(sys.argv) < 2:
+    print "Usage: %s <path_to_unzipped_Packages> <path_to_file_with_multiarch_headers>" % sys.argv[0]
+    sys.exit(1)
+
+package_file = sys.argv[1]
+multiarch_file = sys.argv[2]
+
+packages = {}
+
+with open(package_file, 'r') as pkgs:
+    for pkg in Packages.iter_paragraphs(pkgs):
+        packages[pkg['Package'] + pkg['Version'] + pkg['Architecture']] = pkg
+
+new_package = open(package_file + '.new', 'w')
+with open(multiarch_file, 'r') as m_file:
+    for line in m_file:
+        p, v, a, multi = line.rstrip().split(" ")
+        if packages.has_key(p + v + a):
+            packages[p + v + a]['Multi-Arch'] = multi
+        else:
+            print "Package %s with version %s and architecture %s not found in list." % (p, v, a)
+
+for pkg in packages.values():
+    pkg.dump(new_package)
+    new_package.write("\n")
+new_package.close()            
+sys.exit(0)
+
